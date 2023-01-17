2eb1f6ed0602205364eeaa1f9c39406881112843
Paing Hein Khant <71572306+PaingHeinKhant@users.noreply.github.com>
2023-01-17 15:48:44 +0630

    route and route nesting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
 import { About } from './components/About'
+import { FeaturedProducts } from './components/FeaturedProducts'
 import { Home } from './components/Home'
 import Navbar from './components/Navbar'
+import { NewProduct } from './components/NewProduct'
+import NotFound from './components/NotFound'
+import OrderSummary from './components/OrderSummary'
+import Products from './components/Products'
 
 const App = () => {
   return (
@@ -11,6 +16,13 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='about' element={<About/>}/>
+      <Route path='order-summary' element={<OrderSummary/>}/>
+      <Route path='products' element={<Products/>}>
+        <Route index element={<FeaturedProducts/>}/>
+        <Route path='featured' element={<FeaturedProducts/>}/>
+        <Route path='new' element={<NewProduct/>}/>
+      </Route>
+      <Route path='*' element={<NotFound/>}/>
     </Routes>
     </>
   )
--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-export const Home = () => {
+const OrderSummary = () => {
     const navigate = useNavigate();
   return (
     <>
-    <div>Home</div>
-    <button onClick={()=>navigate('order-summary')}>Order</button>
+    <div>Order Confirmed!!</div>
+    <button onClick={()=>navigate(-1)}>back</button>
     </>
     
   )
 }
+
+export default OrderSummary
