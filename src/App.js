e7a0f91b7a9c0fe081ef831f1ff651b81cbed770
Paing Hein Khant <71572306+PaingHeinKhant@users.noreply.github.com>
2023-01-18 18:15:50 +0630

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes,Route } from 'react-router-dom'
-import { About } from './components/About'
+// import { About } from './components/About'
+import { Admin } from './components/Admin'
 import { FeaturedProducts } from './components/FeaturedProducts'
 import { Home } from './components/Home'
 import Navbar from './components/Navbar'
@@ -8,6 +9,10 @@ import { NewProduct } from './components/NewProduct'
 import NotFound from './components/NotFound'
 import OrderSummary from './components/OrderSummary'
 import Products from './components/Products'
+import { User } from './components/User'
+import UserDetail from './components/UserDetail'
+
+const LazyAbout = React.lazy(()=> import('./components/About'))
 
 const App = () => {
   return (
@@ -15,12 +20,21 @@ const App = () => {
     <Navbar/>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='about' element={<About/>}/>
+      <Route path='about' element={
+        <React.Suspense fallback='Loading...'>
+          <LazyAbout/>
+        </React.Suspense>
+        }
+        />
       <Route path='order-summary' element={<OrderSummary/>}/>
       <Route path='products' element={<Products/>}>
         <Route index element={<FeaturedProducts/>}/>
         <Route path='featured' element={<FeaturedProducts/>}/>
         <Route path='new' element={<NewProduct/>}/>
+      </Route>
+      <Route path='user' element={<User/>}>
+        <Route path=':userId' element={<UserDetail/>}/>
+        <Route path='admin' element={<Admin/>}/>
       </Route>
       <Route path='*' element={<NotFound/>}/>
     </Routes>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export const About = () => {
+const About = () => {
   return (
-    <div>About</div>
+    <div>
+      Nulla porttitor accumsan tincidunt. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Nulla porttitor accumsan tincidunt. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+      Nulla porttitor accumsan tincidunt. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Nulla porttitor accumsan tincidunt. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+      Nulla porttitor accumsan tincidunt. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Donec sollicitudin molestie malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Nulla porttitor accumsan tincidunt. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.
+    </div>
   )
 }
+
+export default About
